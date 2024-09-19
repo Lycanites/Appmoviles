3b60e5d6d1a2cbedf181657f7f73f89430aad580
Andre_XPG <Lycanites@outlook.com>
2024-09-18 20:54:51 -0600

    Declaramos variables

---
+++ script.js
@@ -0,0 +1,37 @@
+//Hernandez Castañeda Andre Alain
+
+alert("ola");
+
+//Comentario en una linea 
+/**
+ * Comentario
+ * mul
+ * ti
+ * li
+ * ne
+ * as
+ */
+
+
+/**
+ * Las variables guardan informacion que puedes
+ * reutilizar 
+ * 
+ */
+
+//* Declaracion  de variables
+
+let nombre = "Andre"
+const PI = 3.141621
+var edad = 25
+
+//Tipos de datos:
+/**
+ * Cadena de texto (String): "Hola, mundo"
+ * Numericos (INT): 25
+ * Booleanos (Bool): True/False
+ * 
+ * 
+ */
+
+
